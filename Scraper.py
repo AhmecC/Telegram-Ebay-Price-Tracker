27523e2e89aca6e3a4e404296790c662e2788603
Aqeel Ahmed <aqeelvahmed@gmail.com>
2022-09-08 16:13:56 +0100

    Update Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 import os
-path = "CHROMEDRIVER PATH"
+path = r""
 
 
 class Scraper:
@@ -13,9 +13,9 @@ class Scraper:
         self.item_list_finder()
         self.item_storage()
 
-        
     def item_list_finder(self):
         """Obtain IDs and respective list of what they're tracking and searches for matches"""
+
         with open("Ids", "r") as file:
             IDs = file.readlines()
             if len(IDs) != 0:  # Proceed only if ID is inside
@@ -28,10 +28,10 @@ class Scraper:
                                 A = i.split(", ")
                                 self.ebay_scraper(A[1], float(A[2]))  # Send Item and Price to actual info gatherer
 
-                                
     def ebay_scraper(self, item, target_price):
-        """Searches item on ebay and collects data inc Hyperlink"""
+        """Searches item on ebay and collects raw DATA inc Hyperlink"""
 
+        # ---------- Search Item on Ebay ---------- #
         self.driver.get("https://www.ebay.co.uk/")
         self.item = item
         sleep(2)
@@ -40,36 +40,39 @@ class Scraper:
         search.send_keys(Keys.ENTER)  # Search up item using saved name
         sleep(4)
 
-        data = self.driver.find_elements(By.CLASS_NAME, "s-item")  # Data found from website
+        # ---------- Obtain Raw Item Data & Hyperlinks ---------- #
+        DATA = self.driver.find_elements(By.CLASS_NAME, "s-item")
         hyperlinks = []
-        for x in range(1, 61):  # Hyperlinks not in data, so we find here
+        for x in range(1, 61):  # Hyperlinks not in DATA
             try:
                 link = self.driver.find_element(By.XPATH, f"/html/body/div[5]/div[4]/div[2]/div[1]/div[2]/ul/li[{x}]/div/div[2]/a")
                 hyperlinks.append(link.get_attribute("href"))
             except:
-                pass
+                link = self.driver.find_element(By.XPATH, f"/html/body/div[5]/div[4]/div[3]/div[1]/div[2]/ul/li[{x}]/div/div[2]/a")
+                hyperlinks.append(link.get_attribute("href"))
 
-        item_info = []
+        # ---------- Extracting Text Data ---------- #
+        TEXT_DATA = []
         count = 0
-        for entry in data[1:]:  # First item is always empty so we ignore that
+        for i in DATA[1:]:  # First item is always empty so we ignore it
             try:
-                info = entry.text  # Get text of each data variable
-                form = info.split("\n")  # Create a list of data variable
-                form.append(hyperlinks[count])
+                fat_txt = i.text  # Obtain full text version of each Ebay Listing
+                text = fat_txt.split("\n")  # Listing seperated into sections
+                text.append(hyperlinks[count])
                 if count != len(hyperlinks):
-                    count += 1  # Makes sure hyperlinks are allocated to correct data piece
-                item_info.append(form)
+                    count += 1  # Makes Sure Hyperlinks allocated to correct listing
+                TEXT_DATA.append(text)  # All listings inside TEXT_DATA
             except:
                 pass
 
-        self.item_sorter(item_info, target_price)
+        self.complete_sorter(TEXT_DATA, target_price)
 
-        
-    def item_sorter(self, itms, target_price):
-        """Stores relevant info for item, and if any results are yielded we move to checker()"""
-        all_organised = []
-        for x in itms:
+    def complete_sorter(self, TEXT_DATA, target_price):
+        """Cleans Data, and finds matches"""
 
+        # ---------- Reorganise to have only Relevant Data ---------- #
+        ORGANISED = []
+        for x in TEXT_DATA:
             # OBTAIN Item_Name #
             if x[0] == "GREAT PRICE":
                 item_name = x[1]
@@ -77,7 +80,6 @@ class Scraper:
                 item_name = x[0][11:]
             else:
                 item_name = x[0]
-
             # OBTAIN Total_Price #
             price = 0
             for entry in x:
@@ -91,7 +93,6 @@ class Scraper:
                         price += 0
                 except:
                     pass
-
             # OBTAIN Item_type #
             for entry in x:
                 try:
@@ -106,61 +107,86 @@ class Scraper:
                 except:
                     item_type = "unknown"
                     pass
+            ORGANISED.append([item_type, item_name, price, x[-1]])
 
-            all_organised.append([item_type, item_name, price, x[-1]])
-
-        # CHECK NUMBER OF SEARCH RESULTS #
+        # ---------- Check Num of Search Results to ensure relevant results are recorded ---------- #
         relevant_items = self.driver.find_element(By.CLASS_NAME, 'srp-controls__count-heading').text
         try:
-            num = int(relevant_items[:3])  # If there is < 61 search results it will give only those results
+            num = int(relevant_items[:3])
+            # If num<61 then only that will be shown
+            # Items >num could be irrelevant to the search
         except:
             num = 0
 
-        if num != 0:
-            try:
-                check_items = all_organised[:num]
-                print(check_items)
-            except:
-                check_items = all_organised
-            self.checker(check_items, target_price)
+        # ---------- Given >0 Search Results we now find Matches ---------- #
+        if num == 0:
+            print("No Search Results")
         else:
-            print("No Search Results")  # Create a Proper Error handler
-
-            
-    def checker(self, items, target_price):
-        """Checks if Items satisfy demands of user"""
-        for i in items:
-            # Appends all data to self list#
-            if i[0][0] == "Auction":
-                self.all.append(f"{self.item}, {i[0][0]}, {i[1]}, {i[2]}, {i[3]}")
-            else:
-                self.all.append(f"{self.item}, {i[0]}, {i[1]}, {i[2]}, {i[3]}")
-
-        satisfied = []
-
-        for i in items:
             try:
-                if i[0][1][1] == "h":
-                    if int(i[0][1][0]) < 6:
+                ORGANISED = ORGANISED[:num] # If num<61, we remove irrelevant search results
+            except:
+                pass
+
+            # ------ Check for Satisfactory Items ------ #
+            SATISFIED = []
+            for i in ORGANISED:
+                try:  # Auction Item satisfied if <6hrs left and <Target
+                    if i[0][1][1] == "h":
+                        if int(i[0][1][0]) < 6:
+                            if int(i[2]) <= target_price:
+                                SATISFIED.append(i)
+                except:  # Item Satisfied if <Target, we ignore Best Offer Items
+                    if i[0] == "Buy it now":
+                        # i[0] == "Best Offer" or
                         if int(i[2]) <= target_price:
-                            satisfied.append(i)
-                            # Auction item satisfied if <Target_£ & <6hrs left #
-            except:
-                if i[0] == "Best Offer" or i[0]== "Buy it now":
-                    if int(i[2]) <= target_price:
-                        satisfied.append(i)
-                        # Item satisfied if <Target_£ #
+                            SATISFIED.append(i)
 
-        # REPORT BACK SATISFIED ITEMS (IF ANY) #
-        if len(satisfied) == 0:
-            print("No Matches")
-        else:
-            for i in satisfied:
-                print(i)
+            # ------ Remove Dodgy Items ------ #
+            SATISFIED = [i for i in SATISFIED if i[2] > 0.5*target_price]
+            SATISFIED = sorted(SATISFIED, key=lambda x: x[2], reverse=False)  # Lowest to Highest Price
 
-                
+            nice = []
+            for x in SATISFIED:
+                price = ""
+                for i in str(x[2]):
+                    if i in ".":
+                        price += f"\{i}"
+                    else:
+                        price += i
+
+                hyper = ""
+                for i in x[3]:
+                    if i in "\'_*[],()~>#+-_=|!":
+                        hyper += f"\{i}"
+                    else:
+                        hyper += i
+
+
+
+                if x[0][0] == "Auction":
+                    nice.append(f"{x[0][0]} Item with {x[0][1][:12]}is currently [£{price}]({hyper})")
+                else:
+                    nice.append(f"{x[0]} Item is [£{price}]({hyper})")
+
+
+            # ------ Report Back Satisfied Items ------ #
+            if len(SATISFIED) != 0:
+                with open(f"{self.id}_send", "w", encoding="utf-8") as file:
+                    file.write(f"{nice[0]}\n{nice[1]}\n{nice[2]}")
+
+
+
+
+            # ------ Add all items to big list ------ #
+            for i in ORGANISED:
+                # Appends all data to self list#
+                if i[0][0] == "Auction":
+                    self.all.append(f"{self.item}, {i[0][0]}, {i[1]}, {i[2]}, {i[3]}")
+                else:
+                    self.all.append(f"{self.item}, {i[0]}, {i[1]}, {i[2]}, {i[3]}")
+
 #     def item_storage(self):
-#         """THIS FUNCTION DOESNT WORK"""
+#         """IRRELEVANT FOR NOW"""
 
 #         if os.stat(f"{self.id}_item_list").st_size == 0:
 #             with open(f"{self.id}_item_list", "a", encoding="utf-8") as file:
@@ -188,3 +214,5 @@ class Scraper:
 #             with open(f"{self.id}_item_list", "a", encoding="utf-8") as fp:
 #                 for i in repeat:
 #                     fp.write(f"\n{i}")
+
+
